4b5906562b1d87cadbab22edfd7136eb4ce26a9c
SarantopoulosKon <33217757+SarantopoulosKon@users.noreply.github.com>
2022-02-14 14:23:40 +0200

    Update @tiledb-inc/tiledb-cloud to v1.0.4 to use getFileContents helper for the file rendering example (#2)

--- src/pages/Files/Files.js
+++ src/pages/Files/Files.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Button, Divider, Typography } from "antd";
 import Client from "@tiledb-inc/tiledb-cloud";
 import CodeSnippet from "../../components/CodeSnippet";
@@ -9,13 +9,6 @@ const tiledbQuery = new Client({
 });
 
 const { Title, Paragraph } = Typography;
-const fileSize = 619181;
-
-const query = {
-  layout: "row-major",
-  ranges: [[0, fileSize]],
-  bufferSize: 5355910,
-};
 
 const markdown = `
 import Client from "@tiledb-inc/tiledb-cloud";
@@ -24,64 +17,38 @@ import Client from "@tiledb-inc/tiledb-cloud";
 const client = new Client({
     apiKey: ''
 });
-// We can get the file size from the array metadata
-const fileSize = 615039
-
-const ranges = [
-    [0, fileSize]
-]
-
-const query = {
-    layout: "row-major",
-    ranges: ranges,
-    bufferSize: 15000000000000,
-};
-
-const contents = []
 
 (async function() {
-  // Iterate over all results in case query is incomplete
-  for await (let results of client.query.ReadQuery("TileDB-Inc", "autzen_tiledb", query)) {
-      contents.push(results.contents);
-  }
-  // Create a buffer from the contents (Array of uint8)
-  const buffer = Uint8Array.from(contents).buffer;
-  saveBufferAsFile(buffer, 'document.pdf');
+  // Get file contents as ArrayBuffer
+  const { buffer, mimeType, originalFileName } = await tiledbQuery.getFileContents(
+    "TileDB-Inc",
+    "VLDB17_TileDB"
+  );
+  
+  renderMimeType(buffer, mimeType);
 })();
 `;
 
 const Files = () => {
-  const [contents, setContents] = React.useState([]);
   const [dataPdf, setPdfData] = React.useState("");
   const [loading, setLoading] = React.useState(false);
 
   const getFile = React.useCallback(() => {
     async function fetchData() {
       setLoading(true);
-      for await (let results of tiledbQuery.query.ReadQuery(
+      const { buffer, mimeType } = await tiledbQuery.getFileContents(
         "TileDB-Inc",
-        "VLDB17_TileDB",
-        query
-      )) {
-        setContents((c) => {
-          return c.concat(results.contents || []);
-        });
-      }
+        "VLDB17_TileDB"
+      );
+      const blob = new Blob([buffer], { type: mimeType });
+      const pdfurl = window.URL.createObjectURL(blob) + "#view=FitW";
+
       setLoading(false);
+      setPdfData(pdfurl);
     }
 
     fetchData();
   }, []);
-
-  useEffect(() => {
-    if (contents.length >= fileSize) {
-      const buffer = Uint8Array.from(contents).buffer;
-      const blob = new Blob([buffer], { type: "application/pdf" });
-      const pdfurl = window.URL.createObjectURL(blob) + "#view=FitW";
-
-      setPdfData(pdfurl);
-    }
-  }, [contents]);
 
   return (
     <>
@@ -90,8 +57,9 @@ const Files = () => {
         <Paragraph>
           Since files (just like TileDB notebooks) are stored as TileDB arrays,
           we can query the array get the contents and show/download the file.
-          Furthermore <b>@tiledb-inc/tiledb-cloud</b> provides a convenient
-          method <b>downloadFile</b> to download files locally.
+          <b>@tiledb-inc/tiledb-cloud</b> provides a convenient methods{" "}
+          <b>downloadFile</b> and <b>getFileContents</b> to download/render
+          files locally.
         </Paragraph>
         <Title level={4}>Example code</Title>
         <CodeSnippet>{markdown}</CodeSnippet>
